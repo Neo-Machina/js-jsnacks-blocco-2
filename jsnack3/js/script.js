17e9159c2e394ab9f44e8d3263ddc74d6de2ecac
Neo-Machina <andreistatache1997@gmail.com>
2022-06-10 14:01:54 +0200

    jsnack3 completed

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,3 +1,29 @@
 // Il software deve chiedere per 5 volte all’utente di inserire un numero.
 // Il programma stampa la somma di tutti i numeri inseriti.
 // Esegui questo programma in due versioni, con il for e con il while.
+
+// FOR VERSION
+// Crea la variabile sumNumbers con valore iniziale uguale a 0
+let sumNumbers = 0;
+for(let i = 0; i < 5; i++) {
+    // Il software chiede all’utente di inserire un numero
+    const userNumber = parseInt( prompt ('Dimmi un numero') );
+    
+    // Somma di tutti i numeri
+    sumNumbers += userNumber;
+}
+
+// Stampo la somma 
+console.log(sumNumbers);
+
+// WHILE VERSION
+// let i = 0;
+// while (i < 5) {
+//     const userNumber = parseInt( prompt ('Dimmi un numero') );
+
+//     sumNumbers += userNumber;
+    
+//     i++;
+// }
+
+// console.log(sumNumbers);
